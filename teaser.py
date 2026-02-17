46f67d47feefffca0f7f1e2fb75cebe25b47683e
tony55551-rgb <tony55551@gmail.com>
2026-02-17 20:52:42 +0530

    Create teaser.py

---
+++ teaser.py
@@ -0,0 +1,3 @@
+<body style="margin:0">
+  <embed src="https://raw.githubusercontent.com/tony55551-rgb/ba-assets/main/Digital%20Transformation_Session%20Teaser_090226%203.pdf" type="application/pdf" width="100%" height="100%">
+</body>
